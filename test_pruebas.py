b56c54e313b84d2e292145dacce93382b5ddee28
SneiderViana <132215643+SneiderViana@users.noreply.github.com>
2023-05-02 19:36:24 -0500

    pruebas fallan
    se relizan las pruebas y se llaman los metodos van a fallar porque no existen

---
+++ test_pruebas.py
@@ -0,0 +1,13 @@
+
+def test_Isprime():
+    numero=cal.Primo(4)
+    assert numero == False
+
+def test_fibinnacci():    
+  num=3
+  numero=cal.fibonnacci(num)
+  assert numero == 2
+
+def test_numero():
+    saludo=cal.hello()
+    assert  saludo== "HELLO FASTAPI"
